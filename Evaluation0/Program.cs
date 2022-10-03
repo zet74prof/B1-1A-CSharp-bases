bb8246a6559ff710ebd6aaad99eea78497a6d51b
Zet74 <etienne.buffet@gmail.com>
2022-10-03 21:11:14 +0200

    correction

---
+++ Evaluation0/Program.cs
@@ -0,0 +1,48 @@
+﻿//déclaration des variables
+decimal tempMin = 36.2m;
+decimal tempMax = 40.4m;
+string etatPerso = "OK";
+bool dead = false;
+decimal tempPerso;
+
+Console.WriteLine("Saisissez votre température: \n");
+//Récupération de la saisie utilisateur
+string saisie = Console.ReadLine();
+
+if (Decimal.TryParse(saisie, out tempPerso))
+{
+    //si la température du perso est inférieure à la température minimum
+    //ou si la température du perso est supérieure à la température maximum
+    //alors le perso est mort
+    if (tempPerso < tempMin || tempPerso > tempMax)
+    {
+        dead = true;
+        etatPerso = "mort";
+        Console.WriteLine($"Vous êtes {etatPerso}\n");
+    }
+    else if (tempPerso < 36m || tempPerso > 38.5m)
+    {
+        etatPerso = "bien malade";
+        Console.WriteLine("Vous êtes " + etatPerso + "\n");
+    }
+    else if (tempPerso < 36.6m || tempPerso > 37.4m)
+    {
+        etatPerso = "un peu malade";
+        Console.WriteLine($"Vous êtes {etatPerso}\n");
+    }
+    else
+    {
+        etatPerso = "en forme";
+        Console.WriteLine("Vous êtes " + etatPerso + "\n");
+    }
+}
+else
+{
+    Console.WriteLine("Saisissez un nombre décimal avec la virgule comme séparateur");
+}
+
+
+
+
+
+
---
+++ Evaluation2/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Entrez votre age: ");
+string ageSaisi =  Console.ReadLine();
+Console.WriteLine("Entrez votre poids en kg (ex: 23,4): ");
+string poidsSaisi = Console.ReadLine();
+bool enfant = false;
+int quantiteDolipraneMaxParJour = 0;
+string msgDoseMax = "Dose quotidienne max: ";
+string msgDoseAdulte = "500 mg ou 1 g de paracétamol, 1 à 3 fois par jour";
+string msgDose4h = " mg 6 fois par jour";
+string msgDose6h = " mg 4 fois par jour";
+Console.WriteLine("Posologie Doliprane :\n");
+
+if (Int32.TryParse(ageSaisi, out int age) && Decimal.TryParse(poidsSaisi, out decimal poids))
+{
+    if (age < 14)
+    {
+        enfant = true;
+    }
+
+    if (!enfant)
+    {
+        quantiteDolipraneMaxParJour = 3000;
+        Console.WriteLine(msgDoseMax + quantiteDolipraneMaxParJour + " mg");
+        Console.WriteLine(msgDoseAdulte);
+    }
+    else
+    {
+        quantiteDolipraneMaxParJour = (int)(poids * 60);
+        Console.WriteLine(msgDoseMax + quantiteDolipraneMaxParJour + " mg");
+        Console.WriteLine($"{quantiteDolipraneMaxParJour/6m} {msgDose4h}");
+        Console.WriteLine($"{quantiteDolipraneMaxParJour/4m} {msgDose6h}");
+    }
+}
