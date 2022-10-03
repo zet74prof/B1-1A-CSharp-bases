45d82d4672e83922b4bf27e19e17b9eb3f53dca9
Zet74 <etienne.buffet@gmail.com>
2022-10-03 23:21:14 +0200

    final

--- Evaluation0/Program.cs
+++ Evaluation0/Program.cs
@@ -1,40 +1,48 @@
 ﻿//déclaration des variables
-decimal tempMin = 36.2m;
+decimal tempMin = 35.2m;
 decimal tempMax = 40.4m;
-string etatPerso = "OK";
+string etatPerso = "en forme";
 bool dead = false;
 decimal tempPerso;
 
-Console.WriteLine("Saisissez votre température: \n");
+Console.WriteLine("Saisissez votre température (ex: 37,4): \n");
 //Récupération de la saisie utilisateur
 string saisie = Console.ReadLine();
 
 if (Decimal.TryParse(saisie, out tempPerso))
 {
-    //si la température du perso est inférieure à la température minimum
-    //ou si la température du perso est supérieure à la température maximum
+    //si la température du perso est inférieure ou égale à la température minimum
+    //ou si la température du perso est supérieure ou égale à la température maximum
     //alors le perso est mort
-    if (tempPerso < tempMin || tempPerso > tempMax)
+    if (tempPerso <= tempMin || tempPerso >= tempMax)
     {
         dead = true;
-        etatPerso = "mort";
-        Console.WriteLine($"Vous êtes {etatPerso}\n");
     }
+    //sinon si la température du perso est strictement inférieure à 36
+    //ou si la température du perso est strictement supérieure à 38.5
     else if (tempPerso < 36m || tempPerso > 38.5m)
     {
         etatPerso = "bien malade";
-        Console.WriteLine("Vous êtes " + etatPerso + "\n");
+        //Console.WriteLine("Vous êtes " + etatPerso + "\n");
     }
+    //sinon si la température du perso est strictement inférieure à 36.6
+    //ou si la température du perso est strictement supérieure à 37.4
     else if (tempPerso < 36.6m || tempPerso > 37.4m)
     {
         etatPerso = "un peu malade";
-        Console.WriteLine($"Vous êtes {etatPerso}\n");
+        //Console.WriteLine($"Vous êtes {etatPerso}\n");
     }
     else
     {
-        etatPerso = "en forme";
-        Console.WriteLine("Vous êtes " + etatPerso + "\n");
+        //Console.WriteLine("Vous êtes " + etatPerso + "\n");
     }
+
+    if (dead)
+    {
+        etatPerso = "mort";
+        //Console.WriteLine("Vous êtes mort");
+    }
+    Console.WriteLine("Vous êtes " + etatPerso + "\n");
 }
 else
 {
--- Evaluation1/Program.cs
+++ Evaluation1/Program.cs
@@ -7,28 +7,28 @@ Console.WriteLine("Saisissez:\n" +
                   "3- pour ajouter des points à votre solde\n" +
                   "9-pour être mis en relation avec un opérateur");
 string saisie = Console.ReadLine();
-var synthesizer = new SpeechSynthesizer();
-synthesizer.SetOutputToDefaultAudioDevice();
+//var synthesizer = new SpeechSynthesizer();
+//synthesizer.SetOutputToDefaultAudioDevice();
 
 if (saisie == "1")
 {
     string text = "Nos bureaux sont ouverts du lundi au vendredi, de 8h à 17h sans interruption";
     Console.WriteLine(text);
-    synthesizer.Speak(text);
+    //synthesizer.Speak(text);
     
 }
 else if (saisie == "2")
 {
-    Console.WriteLine($"Votre solde est de: {solde}€");
+    Console.WriteLine($"Votre solde est de: {solde} points");
 }
 else if (saisie == "3")
 {
-    Console.WriteLine($"Votre solde est de: {solde}€\n" +
+    Console.WriteLine($"Votre solde est de: {solde} points\n" +
                       "Saisissez un nombre de points à ajouter:");
     saisie = Console.ReadLine();
     int pointsAjout = Convert.ToInt32(saisie);
     solde += pointsAjout;
-    Console.WriteLine($"Votre nouveau solde est de: {solde}€");
+    Console.WriteLine($"Votre nouveau solde est de: {solde} points");
 }
 else if (saisie == "9")
 {
